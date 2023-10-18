53f8f3ec1ba757616223ec7337b074721e124ecd
aswin7469 <aswinas@pixysos.com>
2023-10-18 23:03:34 +0530

    Revert "Cleanup smart battery entries (1/3)"
    This reverts commit 742e506d13e4f28e4334d1344816f1df45c0c772.

--- tests/robotests/src/com/android/settings/fuelgauge/PowerUsageFeatureProviderImplTest.java
+++ tests/robotests/src/com/android/settings/fuelgauge/PowerUsageFeatureProviderImplTest.java
@@ -127,6 +127,11 @@ public class PowerUsageFeatureProviderImplTest {
     }
 
     @Test
+    public void testIsAdaptiveChargingSupported_returnFalse() {
+        assertThat(mPowerFeatureProvider.isAdaptiveChargingSupported()).isFalse();
+    }
+
+    @Test
     public void testGetResumeChargeIntentWithoutDockDefender_returnNull() {
         assertThat(mPowerFeatureProvider.getResumeChargeIntent(false)).isNull();
     }
