f81ec98597c6b6c0feb91a64cca0a4eaa0b2d72f
aswin7469 <aswinas@pixysos.com>
2023-12-18 16:40:20 +0530

    [SQUASHED] Settings: Move Google battery and Now playing to pixel-framework
    Revert "Port "Battery Usage Alerts" feature from factory images"
    
    This reverts commit 01f15597641e7be775f1400df5f25b5fb10edc83.
    
    Revert "Settings: Make google's battery widget work"
    
    This reverts commit efbcfdda333a0b28ee03b3c40c73eadc537718ae.
    
    Revert "Revert "Cleanup smart battery entries (1/3)""
    
    This reverts commit b8a4d1571b95bdd3057c771d68fb1f432a010aad.
    
    Revert "Settings: Ambient Music Ticker - Allow to pulse on new tracks [2/2]"
    
    This reverts commit 974e19b35a74b94262d287685fed35a8f6629339.

--- tests/robotests/src/com/android/settings/fuelgauge/PowerUsageFeatureProviderImplTest.java
+++ tests/robotests/src/com/android/settings/fuelgauge/PowerUsageFeatureProviderImplTest.java
@@ -132,11 +132,6 @@ public class PowerUsageFeatureProviderImplTest {
     }
 
     @Test
-    public void testIsAdaptiveChargingSupported_returnFalse() {
-        assertThat(mPowerFeatureProvider.isAdaptiveChargingSupported()).isFalse();
-    }
-
-    @Test
     public void testGetResumeChargeIntentWithoutDockDefender_returnNull() {
         assertThat(mPowerFeatureProvider.getResumeChargeIntent(false)).isNull();
     }
