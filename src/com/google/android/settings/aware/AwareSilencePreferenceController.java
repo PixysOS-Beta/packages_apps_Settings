1d874b97a933bd86c4d10d1ed313687b0553c96f
aswin7469 <aswinas@pixysos.com>
2024-03-21 22:24:07 +0530

    WIP: Motion sense
    Change-Id: I8b876e4dac63aa5ae21c1ffb5fcff88e2b77026b
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

---
+++ src/com/google/android/settings/aware/AwareSilencePreferenceController.java
@@ -0,0 +1,48 @@
+package com.google.android.settings.aware;
+
+import android.content.Context;
+import android.content.IntentFilter;
+import android.provider.Settings;
+import com.android.settings.R;
+
+public class AwareSilencePreferenceController extends AwareBasePreferenceController {
+    public /* bridge */ /* synthetic */ Class getBackgroundWorkerClass() {
+        return super.getBackgroundWorkerClass();
+    }
+
+    public /* bridge */ /* synthetic */ IntentFilter getIntentFilter() {
+        return super.getIntentFilter();
+    }
+
+    public /* bridge */ /* synthetic */ int getSliceHighlightMenuRes() {
+        return super.getSliceHighlightMenuRes();
+    }
+
+    public /* bridge */ /* synthetic */ boolean hasAsyncUpdate() {
+        return super.hasAsyncUpdate();
+    }
+
+    public /* bridge */ /* synthetic */ boolean isPublicSlice() {
+        return super.isPublicSlice();
+    }
+
+    public /* bridge */ /* synthetic */ boolean isSliceable() {
+        return super.isSliceable();
+    }
+
+    public /* bridge */ /* synthetic */ boolean useDynamicSliceSummary() {
+        return super.useDynamicSliceSummary();
+    }
+
+    public AwareSilencePreferenceController(Context context, String str) {
+        super(context, str);
+    }
+
+    public CharSequence getSummary() {
+        return this.mContext.getText(isSilenceGestureEnabled() ? R.string.gesture_silence_on_summary : R.string.gesture_setting_off);
+    }
+
+    private boolean isSilenceGestureEnabled() {
+        return this.mFeatureProvider.isEnabled(this.mContext) && Settings.Secure.getInt(this.mContext.getContentResolver(), "silence_gesture", 1) == 1;
+    }
+}
