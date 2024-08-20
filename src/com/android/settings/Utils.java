maldini03/android_packages_apps_Settings-2

Settings: Add toggle for Custom UI [2/2]
* TODO: Refresh systemUI

Change-Id: I541f18ff25280f7b117aeb949a14f2efc1ab5b5c
Signed-off-by: aswin7469 <aswinas@pixysos.com>

src/com/android/settings/Utils.java
@@ -77,6 +77,7 @@ import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.Data;
 import android.provider.ContactsContract.Profile;
 import android.provider.ContactsContract.RawContacts;
+import android.provider.Settings;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.text.Spannable;
@@ -118,6 +119,8 @@ import com.android.settingslib.widget.AdaptiveIcon;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
+
+import static android.provider.Settings.System.CUSTOM_UI_TOGGLE;
 
 public final class Utils extends com.android.settingslib.Utils {
 
@@ -1220,7 +1223,9 @@ public final class Utils extends com.android.settingslib.Utils {
      */
     @ColorInt
     public static int getHomepageIconColor(Context context) {
-        return getColorAttrDefaultColor(context, android.R.attr.colorAccent);
+        int customUIToggleValue = Settings.System.getInt(context.getContentResolver(),
+                 CUSTOM_UI_TOGGLE, 0);
+        return getColorAttrDefaultColor(context, customUIToggleValue == 1 ? android.R.attr.colorAccent : android.R.attr.textColorSecondary);
     }
 
     /**

src/com/android/settings/display/CustomUIPreferenceController.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2018 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+
+import com.android.settings.DisplaySettings;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import static android.provider.Settings.System.CUSTOM_UI_TOGGLE;
+
+public class CustomUIPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_CUSTOM_UI_TOGGLE = "custom_ui_toggle";
+
+    public CustomUIPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_CUSTOM_UI_TOGGLE;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        int customUIToggleValue = Settings.System.getInt(mContext.getContentResolver(),
+                CUSTOM_UI_TOGGLE, 0);
+        ((SwitchPreference) preference).setChecked(customUIToggleValue != 0);
+    }
+
+    @Override
+    public boolean isAvailable() {
+	return true;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean customUIToggleValue = (Boolean) newValue;
+        Settings.System.putInt(mContext.getContentResolver(), CUSTOM_UI_TOGGLE, customUIToggleValue ? 1 : 0);
+        return true;
+    }
+}

PixysOS-Beta/packages_apps_Settings

Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

src/com/android/settings/Utils.java
@@ -21,6 +21,7 @@ import static android.content.Intent.EXTRA_USER_ID;
 import static android.os.UserManager.USER_TYPE_FULL_SYSTEM;
 import static android.os.UserManager.USER_TYPE_PROFILE_MANAGED;
 import static android.os.UserManager.USER_TYPE_PROFILE_PRIVATE;
+import static android.provider.Settings.System.CUSTOM_UI_TOGGLE;
 import static android.text.format.DateUtils.FORMAT_ABBREV_MONTH;
 import static android.text.format.DateUtils.FORMAT_SHOW_DATE;
 
@@ -82,6 +83,7 @@ import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.Data;
 import android.provider.ContactsContract.Profile;
 import android.provider.ContactsContract.RawContacts;
+import android.provider.Settings;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.text.Spannable;
@@ -1288,7 +1290,9 @@ public final class Utils extends com.android.settingslib.Utils {
      */
     @ColorInt
     public static int getHomepageIconColor(Context context) {
-        return getColorAttrDefaultColor(context, android.R.attr.colorAccent);
+        int customUIToggleValue = Settings.System.getInt(context.getContentResolver(),
+                 CUSTOM_UI_TOGGLE, 0);
+        return getColorAttrDefaultColor(context, customUIToggleValue == 1 ? android.R.attr.colorAccent : android.R.attr.textColorPrimary);
     }
 
     /**

src/com/pixys/settings/preferences/CustomUIPreferenceController.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2018 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.pixys.settings.preferences;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+
+import com.android.internal.util.pixys.PixysUtils;
+import com.android.settings.DisplaySettings;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import static android.provider.Settings.System.CUSTOM_UI_TOGGLE;
+
+public class CustomUIPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_CUSTOM_UI_TOGGLE = "custom_ui_toggle";
+
+    public CustomUIPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_CUSTOM_UI_TOGGLE;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        int customUIToggleValue = Settings.System.getInt(mContext.getContentResolver(),
+                CUSTOM_UI_TOGGLE, 0);
+        ((SwitchPreference) preference).setChecked(customUIToggleValue != 0);
+    }
+
+    @Override
+    public boolean isAvailable() {
+	return true;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean customUIToggleValue = (Boolean) newValue;
+        Settings.System.putInt(mContext.getContentResolver(), CUSTOM_UI_TOGGLE, customUIToggleValue ? 1 : 0);
+        PixysUtils.showSystemUiRestartDialog(mContext);
+        return true;
+    }
+}